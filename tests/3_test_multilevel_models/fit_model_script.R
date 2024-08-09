6dbc3c4be2017e92b309f216d15429c9abd5566f
scienceanna <hughesannae@gmail.com>
2024-08-09 11:31:27 +0100

    fitting 2 conditions with multi level sims

--- tests/3_test_multilevel_models/fit_model_script.R
+++ tests/3_test_multilevel_models/fit_model_script.R
@@ -227,3 +227,100 @@ log_pd_kfold <- gen_test$draws("log_lik", format = "matrix")
 
 elpd_kfold_1_2 <- elpd(log_pd_kfold)
 saveRDS(elpd_kfold_1_2, "scratch/elpd_1_2.rds")
+
+
+#### Simulating multiple conditions
+
+item_class_weights = list(c(0.5, 0.5, 0, 0), 
+                          c(0.7, 0.3, 0, 0))
+
+b_stick = c(0, 2)
+
+rho_delta = c(20, 15)
+sd_rho_delta = 5
+
+rho_psi = c(-1, -1)
+
+abs_dir_tuning = list(kappa = rep(10, 4), theta = rep(1, 4))
+
+d3 <- sim_foraging_people(n_people = 10,
+                         n_conditions = 2,
+                         cond_lab = c("A", "B"),
+                         n_trials_per_cond = 4,
+                         n_item_class = 2, n_item_per_class = 10,
+                         item_class_weights, sd_bA = 0.2,
+                         b_stick = b_stick, sd_b_stick = 1,
+                         rho_delta = rho_delta, sd_rho_delta = sd_rho_delta,
+                         rho_psi = rho_psi, sd_rho_psi = 0.5,
+                         abs_dir_tuning = abs_dir_tuning,
+                         inital_sel_params = inital_sel_params) 
+
+d3$found <- fix_person_and_trial(d3$found)
+d3$stim <- fix_person_and_trial(d3$stim)
+
+saveRDS(d3, "scratch/d_2cond.rds")
+
+d_list <- prep_data_for_stan(d3$found, d3$stim, c("spatial", "item_class"))
+d_list <- add_priors_to_d_list(d_list, modelver = "1.0")
+d_list$n_trials_to_sim <- 1
+
+iter = 200
+mod <- cmdstan_model("../../models/multi_level/FoMo1_0.stan")
+
+fit <- mod$sample(data = d_list, 
+                  chains = 4, parallel_chains = 4, threads = 4,
+                  refresh = 10, 
+                  iter_warmup = iter, iter_sampling = iter,
+                  sig_figs = 3)
+
+fit$save_object("scratch/multi_level_2cond_1_0_tmp.rds")
+
+## model 1.1
+
+rho_delta = c(1,2)
+sd_rho_delta = 0.1
+
+d4 <- sim_foraging_people(n_people = 10,
+                          n_conditions = 2,
+                          cond_lab = c("A", "B"),
+                          n_trials_per_cond = 4,
+                          n_item_class = 2, n_item_per_class = 10,
+                          item_class_weights, sd_bA = 0.2,
+                          b_stick = b_stick, sd_b_stick = 1,
+                          rho_delta = rho_delta, sd_rho_delta = sd_rho_delta,
+                          rho_psi = rho_psi, sd_rho_psi = 0.5,
+                          abs_dir_tuning = abs_dir_tuning,
+                          inital_sel_params = inital_sel_params,
+                          rel_proximity = TRUE) 
+
+d4$found <- fix_person_and_trial(d4$found)
+d4$stim <- fix_person_and_trial(d4$stim)
+
+saveRDS(d4, "scratch/d_2cond_relprox.rds")
+
+d_list <- prep_data_for_stan(d4$found, d4$stim, c("spatial", "item_class"))
+d_list <- add_priors_to_d_list(d_list, modelver = "1.1")
+d_list$n_trials_to_sim <- 1
+
+iter = 200
+mod <- cmdstan_model("../../models/multi_level/FoMo1_1.stan")
+
+fit <- mod$sample(data = d_list, 
+                  chains = 4, parallel_chains = 4, threads = 4,
+                  refresh = 10, 
+                  iter_warmup = iter, iter_sampling = iter,
+                  sig_figs = 3)
+
+fit$save_object("scratch/multi_level_2cond_1_1_tmp.rds")
+
+# model 1.2
+
+mod <- cmdstan_model("../../models/multi_level/FoMo1_2.stan")
+
+fit <- mod$sample(data = d_list, 
+                  chains = 4, parallel_chains = 4, threads = 4,
+                  refresh = 10, 
+                  iter_warmup = iter, iter_sampling = iter,
+                  sig_figs = 3)
+
+fit$save_object("scratch/multi_level_2cond_1_2_tmp.rds")
