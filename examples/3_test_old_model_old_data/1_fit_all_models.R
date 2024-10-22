ef78f3e9b9b9dc83b5fb20b019b32fa8990b3a4e
Alasdair Clarke <alasdair.clarke@gmail.com>
2024-10-22 12:06:21 +0100

    post pred acc calc

--- examples/3_test_old_model_old_data/1_fit_all_models.R
+++ examples/3_test_old_model_old_data/1_fit_all_models.R
@@ -17,4 +17,6 @@ fit_model("clarke2022qjep", fomo_ver = "1.0", mode = "traintest",  iter = iter)
 fit_model("clarke2022qjep", fomo_ver = "1.1", mode = "traintest",  iter = iter) 
 
 
+fit_model("hughes2024rsos", fomo_ver = "1.0", mode = "traintest",  iter = iter) 
+fit_model("hughes2024rsos", fomo_ver = "1.1", mode = "traintest",  iter = iter) 
 
