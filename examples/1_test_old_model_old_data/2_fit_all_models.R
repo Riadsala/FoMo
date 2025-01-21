9d5c4bbaf8389aee491d07806c7c9a9c8ba69dd4
Alasdair Clarke <alasdair.clarke@gmail.com>
2025-01-21 07:57:52 +0000

    Update 2_fit_all_models.R

--- examples/1_test_old_model_old_data/2_fit_all_models.R
+++ examples/1_test_old_model_old_data/2_fit_all_models.R
@@ -3,28 +3,21 @@ library(cmdstanr)
 
 source("../../functions/import_data.R")
 source("../../functions/prep_data.R")
+source("../../functions/fit_model.R")
 
 
 iter <- 500
 
-fit_model("kristjansson2014plos", fomo_ver = "1.0", mode = "traintest",  iter = iter)
-fit_model("kristjansson2014plos", fomo_ver = "1.1", mode = "traintest",  iter = iter) 
-fit_model("kristjansson2014plos", fomo_ver = "1.2", mode = "traintest",  iter = iter) 
-
-fit_model("tagu2022cog", fomo_ver = "1.0", mode = "traintest",  iter = iter) 
-fit_model("tagu2022cog", fomo_ver = "1.1", mode = "traintest",  iter = iter) 
-fit_model("tagu2022cog", fomo_ver = "1.2", mode = "traintest",  iter = iter) 
-
 
 fit_model("hughes2024rsos", fomo_ver = "1.0", mode = "traintest",  iter = iter) 
-fit_model("hughes2024rsos", fomo_ver = "1.1", mode = "traintest",  iter = iter) 
-fit_model("hughes2024rsos", fomo_ver = "1.2", mode = "traintest",  iter = iter) 
-
-
-fit_model("clarke2022qjep", fomo_ver = "1.0", mode = "traintest",  iter = iter) 
-fit_model("clarke2022qjep", fomo_ver = "1.1", mode = "traintest",  iter = iter) 
-fit_model("clarke2022qjep", fomo_ver = "1.2", mode = "traintest",  iter = iter) 
-
-fit_model("hughes2024rsos", fomo_ver = "0.0", mode = "traintest", iter = iter) 
-fit_model("tagu2022cog", fomo_ver = "0.0", mode = "traintest", iter = iter) 
-fit_model("kristjansson2014plos", fomo_ver = "0.0", mode = "traintest", iter = iter)
+# fit_model("hughes2024rsos", fomo_ver = "1.1", mode = "traintest",  iter = iter) 
+# fit_model("hughes2024rsos", fomo_ver = "1.2", mode = "traintest",  iter = iter) 
+# 
+# 
+# fit_model("clarke2022qjep", fomo_ver = "1.0", mode = "traintest",  iter = iter) 
+# fit_model("clarke2022qjep", fomo_ver = "1.1", mode = "traintest",  iter = iter) 
+# fit_model("clarke2022qjep", fomo_ver = "1.2", mode = "traintest",  iter = iter) 
+# 
+# fit_model("hughes2024rsos", fomo_ver = "0.0", mode = "traintest", iter = iter) 
+# fit_model("tagu2022cog", fomo_ver = "0.0", mode = "traintest", iter = iter) 
+# fit_model("kristjansson2014plos", fomo_ver = "0.0", mode = "traintest", iter = iter)
