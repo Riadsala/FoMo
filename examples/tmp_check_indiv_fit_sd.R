298cc20f1111ba321c80d7681e7534afab6687a4
Alasdair Clarke <alasdair.clarke@gmail.com>
2025-03-07 09:59:25 +0000

    Update tmp_check_indiv_fit_sd.R

--- examples/tmp_check_indiv_fit_sd.R
+++ examples/tmp_check_indiv_fit_sd.R
@@ -27,3 +27,37 @@ folder <- paste0(sf, "/post/", dataset, "/")
 m <- readRDS(paste0("1_fit_models/scratch/models/", dataset, "/train", model_ver, ".model"))
 post <- extract_post(m, d)
 plot_model_random(post)
+
+# plot variances
+prior <- tibble(value = rexp(1000, 1))
+
+# calculate sd from the post indiv diff estimates
+post$random %>%
+  select(-u_psi) %>%
+  pivot_longer(c(u_a, u_stick, u_delta), names_to = "param") %>%
+  group_by(.draw, param, condition) %>%
+  summarise(sd = sd(value)) -> calc_from_u
+
+post$variances %>% 
+  mutate(param = str_replace_all(param, "b|rho", "u")) %>%
+  ggplot(aes(value)) +
+  geom_density(data = prior, fill = "grey") +
+  geom_density(aes(fill = condition), alpha = 0.5) + 
+  stat_intervalh(data = calc_from_u, aes(colour = condition, x = sd), y = 0) + 
+  facet_wrap(~param)
+
+
+# now repeat for the theta
+prior <- tibble(value = rexp(1000, 1))
+
+post$utheta %>%
+  group_by(.draw, comp, condition) %>%
+  summarise(sd = sd(theta)) -> calc_from_u
+
+post$theta %>%
+  ggplot(aes(sigma)) +
+  geom_density(data = prior, aes(value), fill = "grey") +
+  geom_density(aes(fill = condition), alpha = 0.5) + 
+  stat_intervalh(data = calc_from_u, aes(colour = condition, x = sd), y = 0) + 
+  facet_wrap(~comp)
+
