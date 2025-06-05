5bd7340587627f6e845644520d54d234bb254342
Alasdair Clarke <a.clarke@essex.ac.uk>
2025-06-05 15:04:39 +0100

    Update create_plot1.R

--- figures_for_paper/create_plot1.R
+++ figures_for_paper/create_plot1.R
@@ -19,48 +19,28 @@ plot_a_trial <- function(ds, df,
   # Path vertices are labelled to indicate the order in which they were selected
   # Path segments may be labelled with some feature (a column in df).
   
+  trl = trial
+  ds <- filter(ds, trial == trl)
+  df <- filter(df, trial == trl)
+  
   ds %>% mutate(item_class = factor(item_class)) -> ds
   
   # plot basic trial
   plt <- ggplot(data = ds, aes(x, y)) + 
-    geom_path(data = df, colour = "grey", group = 1) +
-    geom_point(size = 5, aes(colour = item_class, shape = item_class)) +
-    geom_label_repel(data = df, aes(label = found), size = 3) + 
+    geom_path(data = df, colour = "grey20", group = 1) +
+    geom_point(size = 8, aes(colour = item_class, shape = item_class)) +
+    geom_text(data = df, aes(label = found), size = 3) + 
     scale_colour_manual(values = c(18, 15, 3, 4)) + 
     scale_shape_manual(values = c(15, 19, 3, 4))
-  
-  # if segLabel has been left unspecified, add a simple geom_path to connect selected items
-  if (is_null(segLabel)) {
-    
-    plt <- plt 
-    
-  } else { # use geom_labelsegment..
-    
-    #first we have to rearrange things a little
-    df %>% mutate(x0 = lag(x), y0 = lag(y)) -> df
-    
-    # turn labels into characeters and repace NA with a dash (-)
-    df[[segLabel]] = as.character(round(df[[segLabel]], 2))
-    df[[segLabel]][is.na(df[[segLabel]])]  = "-"
-    
-    plt <- plt + geom_textsegment(data = df, 
-                                  aes(x = x0, y = y0, 
-                                      xend = x, yend = y,
-                                      label = .data[[segLabel]]), 
-                                  colour = "grey")
-  }
   
   plt <- plt + coord_equal() + 
     theme(axis.title = element_blank(),
           axis.ticks  = element_blank(),
           axis.text = element_blank(),
           legend.position = "none",
-          plot.background = element_rect(fill='darkgrey', colour='black'),
-          panel.grid = element_blank())
-  
-  if (!is.na(filename)) {
-    ggsave(filename)
-  }
+          # plot.background = element_rect(fill='darkgrey', colour='black'),
+          panel.grid = element_blank(),
+          panel.background =  element_rect(fill='darkgrey', colour='darkgrey'))
   
   return(plt)
 }
