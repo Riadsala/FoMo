02eff1008db8e1d2df13efe107ce621f7f96e00f
Alasdair Clarke <a.clarke@essex.ac.uk>
2025-06-13 15:55:51 +0100

    draft fig6

---
+++ figures_for_paper/create_plot6_example_selections.R
@@ -0,0 +1,99 @@
+library(tidyverse)
+library(patchwork)
+library(circular)
+
+source("../functions/import_data.R")
+source("../functions/prep_data.R")
+source("../functions/compute_summary_stats.R")
+source("../functions/plot_model.R")
+source("../functions/plot_data.R")
+source("../functions/post_functions.R")
+source("../functions/sim_foraging_data.R")
+
+options(mc.cores = 1, digits = 2)
+# set global ggplot theme
+theme_set(theme_bw())
+sf <- "../examples/1_fit_models/scratch"
+
+# select dataset
+ds <- "clarke2022qjep"
+
+# customised plot_a_trial function
+plot_a_trial <- function(ds, df, 
+                         trial = NA, segLabel = NULL,
+                         filename = NA,
+                         draws_to_plot = 1,
+                         pred, ff) {
+  
+  # This function plots a trial: points indicate all items and a path 
+  # joins the items up in the order in which they where selected.
+  # Path vertices are labelled to indicate the order in which they were selected
+  # Path segments may be labelled with some feature (a column in df).
+  
+  trl = trial
+  ds <- filter(ds, trial == trl)
+  df <- filter(df, trial == trl)
+  
+  ds %>% mutate(item_class = factor(item_class)) -> ds
+  
+  # plot basic trial
+  plt <- ggplot(data = ds, aes(x, y)) + 
+    geom_path(data = df, colour = "grey80", group = 1) -> plt
+  
+  df %>% filter(found == ff-1) -> dff
+  
+  # add predictions
+  pred %>% filter(found == ff, trial_p == trl ) %>%
+    group_by(P, model) %>%
+    summarise(n = n()) %>%
+    rename(id = "P") %>%
+    left_join(ds, by = join_by(id)) %>%
+    mutate(x0 = dff$x, y0 = dff$y) %>%
+    filter(n > 2) -> predf
+  
+  plt + geom_segment(data = predf, 
+                     aes(x=x0, y=y0, xend = x, yend = y, group = id, linewidth = n, color = model),
+                     alpha = 0.5)  -> plt
+  
+  print(predf %>% group_by(model) %>% summarise(prop_acc_for = sum(n)/max(pred$.draw)))
+  
+  ds1 <- filter(ds, item_class == 1)
+  ds2 <- filter(ds, item_class == 2)
+  
+   plt + geom_point(data = ds1, size = 5, aes(shape = item_class), color = "#A1CAF1") +
+     geom_point(data = ds2, size = 5, aes(shape = item_class), color = "#BE0032") +
+    geom_text(data = df, aes(label = found), size = 2.5) + 
+     scale_color_paletteer_d("wesanderson::Chevalier1") +
+    scale_shape_manual(values = c(19, 19, 3, 4)) -> plt
+  
+  plt <- plt + coord_equal() + 
+    facet_wrap(~model) +
+    scale_linewidth(guide = "none") + 
+    scale_shape(guide = "none") +
+    theme(axis.title = element_blank(),
+          axis.ticks  = element_blank(),
+          axis.text = element_blank(),
+          # plot.background = element_rect(fill='darkgrey', colour='black'),
+          panel.grid = element_blank(),
+          panel.background =  element_rect(fill='darkgrey', colour='darkgrey'))
+
+  return(plt)
+}
+
+# read in data and predictions
+d <- import_data(ds)
+folder <- paste0(sf, "/post/", ds, "/")
+
+pred10 <- readRDS(paste0(folder, "pred_1_0.rds"))$itemwise %>% 
+  mutate(model = "1.0")
+pred13 <- readRDS(paste0(folder, "pred_1_3.rds"))$itemwise %>% 
+  mutate(model = "1.3")
+
+plt1 <- plot_a_trial(d$stim, d$found, trial = 1273, pred = bind_rows(pred10, pred13), ff = 8)
+
+plt2 <- plot_a_trial(d$stim, d$found, trial = 211, pred = bind_rows(pred10, pred13), ff = 8)
+
+plt3 <- plot_a_trial(d$stim, d$found, trial = 53, pred = bind_rows(pred10, pred13), ff = 8)
+
+plt3 / plt1 / plt2 + plot_layout(guides = "collect")
+
