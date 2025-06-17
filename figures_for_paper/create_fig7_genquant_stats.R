cd808e478417b844ce3c70b458556c5934ce442b
Alasdair Clarke <alasdair.clarke@gmail.com>
2025-06-17 17:00:28 +0100

    More figures for paper

--- figures_for_paper/create_fig7_genquant_stats.R
+++ figures_for_paper/create_fig7_genquant_stats.R
@@ -1,82 +1,14 @@
 library(tidyverse)
-library(cmdstanr)
 library(patchwork)
-library(tidybayes)
 
-source("../../functions/import_data.R")
-source("../../functions/prep_data.R")
-source("../../functions/compute_summary_stats.R")
-source("../../functions/plot_model.R")
-source("../../functions/plot_data.R")
-source("../../functions/post_functions.R")
-source("../../functions/sim_foraging_data.R")
+source("../functions/import_data.R")
+# source("../functions/compute_summary_stats.R")
+# source("../functions/plot_model.R")
+# source("../functions/plot_data.R")
+# source("../functions/post_functions.R")
+# source("../functions/sim_foraging_data.R")
 
 options(mc.cores = 1, digits = 2)
-
-model_ver <- "1_3"
-dataset <- "hughes2024rsos"
-
-# read in data
-d <- import_data(dataset)
-
-sf <- "1_fit_models/scratch"
-folder <- paste0(sf, "/post/", dataset, "/")
-
-#############################################################################
-# plot model comparison over models
-#############################################################################
-plt_hughes <- plot_models_accuracy("hughes2024rsos", scratch_folder = sf) + theme_bw() + ggtitle("Hughes et al (2024, RSOS)")
-plt_clarke <- plot_models_accuracy("clarke2022qjep", scratch_folder = sf) + theme_bw() + ggtitle("Clarke et al (2022, QJEP)")
-
-
-plt_hughes / plt_clarke
-
-# scatter plot of person acc by model
-v1 <- "1_0"
-v2 <- "1_3"
-
-plot_model_accuracy_comparison("hughes2024rsos", v1, v2, scratch_folder = sf) -> plt_acc_comp
-
-#############################################################################
-# improvement throughout a trial?
-#############################################################################
-
-acc <- tibble()
-  
-  folder <- paste0(sf, "/post/", dataset, "/")
-  
-  acc1 <- readRDS(paste0(folder, "pred_", v1, ".rds"))$itemwise %>%
-    mutate(version = v1) %>%
-    filter(found > 1, found < 40) %>%
-    group_by(version, condition, found, .draw) %>%
-    summarise(accuracy = mean(model_correct),
-              .groups = "drop_last") %>%
-    median_hdci(accuracy)
-  
-  acc2 <- readRDS(paste0(folder, "pred_", v2, ".rds"))$itemwise %>%
-    mutate(version = v2) %>%
-    filter(found > 1, found < 40) %>%
-    group_by(version, condition, found, .draw) %>%
-    summarise(accuracy = mean(model_correct),
-              .groups = "drop_last") %>%
-    median_hdci(accuracy)
-  
-  acc <-bind_rows(acc1, acc2)
-
-rm(acc1, acc2)
-
-acc %>% 
-  mutate(version = str_replace(version, "_", "."),
-         version = factor(version, levels = c("1.3", "1.0"))) %>%
-  ggplot(aes(found, ymin = .lower, ymax = .upper,  fill = version)) +
-                 geom_lineribbon(alpha = 0.5) +
-  facet_wrap(~condition) +
-  scale_x_continuous("item", limits = c(1, 20), expand = c(0,0)) +
-  scale_y_continuous("accuracy") -> plt_found
-
-plt_acc_comp / plt_found
-ggsave("acc_comp.pdf", width = 6, height = 5)
-
 
 #############################################################################
 # generated quantities
@@ -88,7 +20,7 @@ rl <- tibble()
 for (ds in datasets) {
   
   rl <- bind_rows(rl,
-                  read_csv(paste0("../1_fit_models/scratch/post/", ds, "/run_statistics.csv")) %>%
+                  read_csv(paste0("../examples/1_fit_models/scratch/post/", ds, "/run_statistics.csv")) %>%
                     mutate(dataset = ds))
 }
 
--- figures_for_paper/create_plot8_starting_location.R
+++ figures_for_paper/create_plot8_starting_location.R
@@ -40,11 +40,7 @@ plot_a_trial <- function(ds, df,
   plt <- ggplot(data = ds, aes(x, y))
   
   # add predictions
-  pred %>% filter(trial_p == trl, .draw %in% c(1,2,3)) %>%
-    bind_rows(bind_rows(df %>% mutate(.draw = 1), 
-                        df %>% mutate(.draw = 2), 
-                        df %>% mutate(.draw = 3)) %>% 
-                mutate(first_selection = "human")) -> predf
+  pred %>% filter(trial_p == trl, .draw %in% c(1,2,3)) -> predf
   
   plt + geom_path(data = predf, 
                      aes(x, y),
@@ -69,8 +65,34 @@ plot_a_trial <- function(ds, df,
           # plot.background = element_rect(fill='darkgrey', colour='black'),
           panel.grid = element_blank(),
           panel.background =  element_rect(fill='darkgrey', colour='darkgrey'))
+  
+  
+  plt2 <- ggplot(data = ds, aes(x, y)) +
+    geom_path(data = df) 
+  
+  ds1 <- filter(ds, item_class == 1)
+  ds2 <- filter(ds, item_class == 2)
+  
+  plt2 + geom_point(data = ds1, size = 5, aes(shape = item_class), color = "#A1CAF1") +
+    geom_point(data = ds2, size = 5, aes(shape = item_class), color = "#BE0032") +
+    geom_text(data = df, aes(label = found), size = 2.5) + 
+    scale_color_paletteer_d("wesanderson::Chevalier1") +
+    scale_shape_manual(values = c(19, 19, 3, 4)) -> plt2
+  
+  plt2 <- plt + coord_equal() + 
+    scale_linewidth(guide = "none") + 
+    scale_shape(guide = "none") +
+    theme(axis.title = element_blank(),
+          axis.ticks  = element_blank(),
+          axis.text = element_blank(),
+          # plot.background = element_rect(fill='darkgrey', colour='black'),
+          panel.grid = element_blank(),
+          panel.background =  element_rect(fill='darkgrey', colour='darkgrey'))
+  
+  
+  pltout <- plt2 + plt
 
-  return(plt)
+  return(pltout)
 }
 
 # read in data and predictions
@@ -85,4 +107,5 @@ bind_rows(
 
 plot_a_trial(d$stim, d$found, trial = 16, pred = pred)
 
+ggsave("figs/fig8_examples.png", width = 8, height = 8)
 
