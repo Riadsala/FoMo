e30ac5ee964520cf4f2d7a764f882b375ccd77e9
scienceanna <hughesannae@gmail.com>
2025-08-27 10:58:23 +0100

    more tidying of everything into one nice script

--- examples/script_to_make_model_plots.R
+++ examples/script_to_make_model_plots.R
@@ -3,6 +3,7 @@ library(this.path)
 
 # set the model version & dataset you want to generate graphs for
 model_ver <- "1_3"
+model_comp <- "1_0" # for the plot_model_accuracy_comparison plot i.e. how does your chosen model compare against another? 1.0 is a sensible baseline.
 dataset <- "kristjansson2014plos"
 free_or_fixed <- "f" # "v" for free, "f" for fixed
 
@@ -11,7 +12,8 @@ filename <- paste0("model_plots_", dataset, "_", free_or_fixed, model_ver, ".htm
 
 
 # render output file
-quarto_render("helper/make_model_plots.qmd", output_file = filename, execute_params = list(model_ver = model_ver, 
+quarto_render("helper/make_model_plots.qmd", output_file = filename, execute_params = list(model_ver = model_ver,
+                                                                                           model_comp = model_comp,
                                                                                       dataset = dataset,
                                                                                       free_or_fixed = free_or_fixed),
               execute_dir = getwd())
