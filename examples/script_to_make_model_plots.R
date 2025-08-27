f200d9ac942aac951fd505ed01f50761293c2ea2
scienceanna <hughesannae@gmail.com>
2025-08-27 10:23:24 +0100

    a ton of faffing to set up a little model plots HTML generator

---
+++ examples/script_to_make_model_plots.R
@@ -0,0 +1,23 @@
+library(quarto)
+
+# set the model version & dataset you want to generate graphs for
+model_ver <- "1_3"
+dataset <- "kristjansson2014plos"
+free_or_fixed <- "v" # "v" for free, "f" for fixed
+
+# some naming of things
+filename <- paste0("model_plots_", dataset, "_", free_or_fixed, model_ver, ".html")
+
+
+# render output file
+quarto_render("helper/make_model_plots.qmd", output_file = filename, execute_params = list(model_ver = model_ver, 
+                                                                                      dataset = dataset,
+                                                                                      free_or_fixed = free_or_fixed),
+              execute_dir = getwd())
+
+# move generated file to a better folder
+setwd('helper')
+stopifnot(file.exists(filename))
+# move filename to path.out
+path.out <- '../reports';
+file.rename(filename, file.path(path.out, filename))
