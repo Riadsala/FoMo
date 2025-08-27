05d464861ba9c2d737099c5a43d43e41511a5f06
scienceanna <hughesannae@gmail.com>
2025-08-27 10:49:28 +0100

    script to make table 2

--- examples/script_to_make_model_plots.R
+++ examples/script_to_make_model_plots.R
@@ -1,9 +1,10 @@
 library(quarto)
+library(this.path)
 
 # set the model version & dataset you want to generate graphs for
 model_ver <- "1_3"
 dataset <- "kristjansson2014plos"
-free_or_fixed <- "v" # "v" for free, "f" for fixed
+free_or_fixed <- "f" # "v" for free, "f" for fixed
 
 # some naming of things
 filename <- paste0("model_plots_", dataset, "_", free_or_fixed, model_ver, ".html")
@@ -21,3 +22,5 @@ stopifnot(file.exists(filename))
 # move filename to path.out
 path.out <- '../reports';
 file.rename(filename, file.path(path.out, filename))
+# set working directory back to here, in case you want to repeat
+setwd(this.path::here())
